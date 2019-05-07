545607bc86e5d8317d9e94ee8c7dd224ed755e5e
Joomy Korkut <joomy@cattheory.com>
2019-05-07 14:54:01 -0400

    Update

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE LambdaCase #-}
 module Lib where
 
+class Parse a where
+  parse :: String -> Maybe a
+
 data Otto =
     Elif
   | Be
@@ -37,77 +40,80 @@ data Otto =
   | Ye
   deriving (Show, Eq, Enum)
 
-renderOtto :: Otto -> String
-renderOtto = \case
-  Elif -> "ا"
-  Be   -> "ب"
-  Pe   -> "پ"
-  Te   -> "ت"
-  Se   -> "ث"
-  Cim  -> "ج"
-  Çim  -> "چ"
-  Ha   -> "ح"
-  Hı   -> "خ"
-  Dal  -> "د"
-  Zel  -> "ذ"
-  Re   -> "ر"
-  Ze   -> "ز"
-  Je   -> "ژ"
-  Sin  -> "س"
-  Şın  -> "ش"
-  Sad  -> "ص"
-  Dad  -> "ض"
-  Tı   -> "ط"
-  Zı   -> "ظ"
-  Ayn  -> "ع"
-  Gayn -> "غ"
-  Fe   -> "ف"
-  Kaf  -> "ق"
-  Kef  -> "ك"
-  Gef  -> "گ"
-  Nef  -> "ڭ"
-  Lam  -> "ل"
-  Mim  -> "م"
-  Nun  -> "ن"
-  Vav  -> "و"
-  He   -> "ه"
-  Ye   -> "ی"
+instance Parse Otto where
+  parse = \case
+    "ا" -> Just Elif
+    "ب" -> Just Be
+    "پ" -> Just Pe
+    "ت" -> Just Te
+    "ث" -> Just Se
+    "ج" -> Just Cim
+    "چ" -> Just Çim
+    "ح" -> Just Ha
+    "خ" -> Just Hı
+    "د" -> Just Dal
+    "ذ" -> Just Zel
+    "ر" -> Just Re
+    "ز" -> Just Ze
+    "ژ" -> Just Je
+    "س" -> Just Sin
+    "ش" -> Just Şın
+    "ص" -> Just Sad
+    "ض" -> Just Dad
+    "ط" -> Just Tı
+    "ظ" -> Just Zı
+    "ع" -> Just Ayn
+    "غ" -> Just Gayn
+    "ف" -> Just Fe
+    "ق" -> Just Kaf
+    "ك" -> Just Kef
+    "گ" -> Just Gef
+    "ڭ" -> Just Nef
+    "ل" -> Just Lam
+    "م" -> Just Mim
+    "ن" -> Just Nun
+    "و" -> Just Vav
+    "ه" -> Just He
+    "ی" -> Just Ye
+    _ -> Nothing
 
-parseOtto :: String -> Otto
-parseOtto = \case
-  "ا" -> Elif
-  "ب" -> Be
-  "پ" -> Pe
-  "ت" -> Te
-  "ث" -> Se
-  "ج" -> Cim
-  "چ" -> Çim
-  "ح" -> Ha
-  "خ" -> Hı
-  "د" -> Dal
-  "ذ" -> Zel
-  "ر" -> Re
-  "ز" -> Ze
-  "ژ" -> Je
-  "س" -> Sin
-  "ش" -> Şın
-  "ص" -> Sad
-  "ض" -> Dad
-  "ط" -> Tı
-  "ظ" -> Zı
-  "ع" -> Ayn
-  "غ" -> Gayn
-  "ف" -> Fe
-  "ق" -> Kaf
-  "ك" -> Kef
-  "گ" -> Gef
-  "ڭ" -> Nef
-  "ل" -> Lam
-  "م" -> Mim
-  "ن" -> Nun
-  "و" -> Vav
-  "ه" -> He
-  "ی" -> Ye
+data Mod =
+    Hemze
+  | Üstün
+  | Esre
+  | Ötre
+  | Şedde
+  | Sükun
+  | Medde
+  | TenvinÜstün -- ^ Also known as "fethateyn" (iki üstün)
+  | TenvinEsre  -- ^ Also known as "kesrateyn" (iki esre)
+  | TenvinÖtre  -- ^ Also known as "dammeteyn" (iki ötre)
+
+instance Parse Mod where
+  parse = \case
+    "\1611" -> Just TenvinÜstün
+    "\1612" -> Just TenvinÖtre
+    "\1613" -> Just TenvinEsre
+    "\1614" -> Just Üstün
+    "\1615" -> Just Ötre
+    "\1616" -> Just Esre
+    "\1617" -> Just Şedde
+    "\1618" -> Just Sükun
+    "\1764" -> Just Medde
+    _ -> Nothing
+
+data OttoModified =
+    PureOtto Otto
+  | ModifiedOtto [Mod] Otto
+
+instance Parse OttoModified where
+  parse = \case
+    -- Exception cases for the special Unicode characters
+    -- that stand for an Ottoman leter with a modification
+    "\1570" -> Just $ ModifiedOtto [Medde] Elif
+    "\1571" -> Just $ ModifiedOtto [Hemze] Elif -- Hemze on top
+    "\1573" -> Just $ ModifiedOtto [Hemze] Elif -- Hemze at bottom
+    _ -> Nothing
 
 data Suffix =
     Ler -- ler, lar
@@ -116,7 +122,6 @@ data Suffix =
   | Miş -- miş, mış, muş, müş
   | Ecek -- ecek, acak
   deriving (Show, Eq, Enum)
-
 
 suffixOttoman :: Suffix -> [[Otto]]
 suffixOttoman = \case
